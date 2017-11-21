d800315ed250f16f9e0bcca6cfa40fe99e9311a5
lightsage88 <adrian.e.rosales@gmail.com>
2017-11-20 21:47:49 -0800

    wrote first small test

--- test/test-server.js
+++ test/test-server.js
@@ -5,3 +5,33 @@ const {app, runServer, closeServer} = require('../server');
 const should = chai.should();
 
 chai.use(chaiHttp);
+
+describe('Blog Machine', function(){
+
+before(function(){
+	return runServer();
+});
+
+after(function(){
+	return closeServer();
+});
+
+	it('should list blog posts on GET', function(){
+		return chai.request(app)
+		.get('/blog')
+		.then(function(res){
+			res.should.have.status(200);
+			res.should.be.json;
+			res.body.should.be.a('object');
+			// res.body.should.include.keys('id', 'content','title');
+			res.body.length.should.be.at.least(1);
+			const expectedKeys = ['id', 'content', 'author'];
+			res.body.forEach(function(post){
+				post.should.be.a('object');
+				post.should.include.keys(expectedKeys);
+			});
+		});
+
+	});
+
+});
